07e0fc44fd56188920ebdfada8c297fa726eb409
Novanda Ahsan <novandaahsan1@gmail.com>
2022-05-30 17:00:24 +0700

    use history in coin categories

--- components/ui/CoinCategories.tsx
+++ components/ui/CoinCategories.tsx
@@ -1,17 +1,20 @@
 import { IonRouterLink } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
 import useCategories from '../../hooks/useCategories';
 
 type Props = {};
 
 const CoinCategories: React.FC<Props> = () => {
   const { categories } = useCategories();
+  const { push } = useHistory();
+
   return (
     <div className="overflow-x-auto overflow-y-hidden -mx-4 my-10 pl-4 py-1">
       <div className="flex flex-wrap -m-2" style={{ width: '900%' }}>
         {categories &&
           categories.map(cat => {
             return (
-              <IonRouterLink key={cat.id} href={`/tabs/search/${cat.id}`}>
+              <IonRouterLink key={cat.id} onClick={() => push(`/tabs/search/${cat.id}`)}>
                 <div className="px-4 hover:bg-gray-100 transition ease-in-out py-2 rounded-full border border-gray-200 mx-1 my-1.5">
                   {cat.name}
                 </div>
