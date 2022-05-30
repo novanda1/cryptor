19f72e6278ef16fe33a4c299514d05fc832c1822
Novanda Ahsan <novandaahsan1@gmail.com>
2022-05-30 11:13:51 +0700

    change route url

--- components/pages/Tabs.tsx
+++ components/pages/Tabs.tsx
@@ -13,30 +13,30 @@ const Tabs = () => {
   return (
     <IonTabs>
       <IonRouterOutlet>
-        <Route path="/tabs/feed" component={Home} exact={true} />
-        <Route path="/tabs/lists" component={Lists} exact={true} />
-        <Route path="/tabs/lists/:listId" component={ListDetail} exact={true} />
-        <Route path="/tabs/settings" component={Settings} exact={true} />
-        <Route path="/tabs" render={() => <Redirect to="/tabs/feed" />} exact={true} />
+        <Route path="/tabs/home" component={Home} exact={true} />
+        <Route path="/tabs/portfolio" component={Lists} exact={true} />
+        <Route path="/tabs/portfolio/:listId" component={ListDetail} exact={true} />
+        <Route path="/tabs/search" component={Settings} exact={true} />
+        <Route path="/tabs" render={() => <Redirect to="/tabs/home" />} exact={true} />
       </IonRouterOutlet>
       <IonTabBar
         slot="bottom"
         className="bg-transparent h-16"
         style={{ boxShadow: '0px -20px 50px rgba(155, 155, 155, 0.1)' }}
       >
-        <IonTabButton className="bg-transparent" tab="tab1" href="/tabs/feed">
+        <IonTabButton className="bg-transparent" tab="tab1" href="/tabs/home">
           <FiHome width={24} height={24} style={{ width: 24, height: 24 }} />
         </IonTabButton>
-        <IonTabButton className="bg-transparent" tab="tab2" href="/tabs/lists">
+        <IonTabButton className="bg-transparent" tab="tab2" href="/tabs/portfolio">
           <FiActivity width={24} height={24} style={{ width: 24, height: 24 }} />
         </IonTabButton>
-        <IonTabButton className="bg-transparent" tab="tab3" href="/tabs/settings">
+        <IonTabButton className="bg-transparent" tab="tab3" href="/tabs/search">
           <FiSearch width={24} height={24} style={{ width: 24, height: 24 }} />
         </IonTabButton>
-        <IonTabButton className="bg-transparent" tab="tab3" href="/tabs/settings">
+        <IonTabButton className="bg-transparent" tab="tab3" href="/tabs/search">
           <RiMoneyDollarBoxLine width={24} height={24} style={{ width: 24, height: 24 }} />
         </IonTabButton>
-        <IonTabButton className="bg-transparent" tab="tab3" href="/tabs/settings">
+        <IonTabButton className="bg-transparent" tab="tab3" href="/tabs/search">
           <FiUser width={24} height={24} style={{ width: 24, height: 24 }} />
         </IonTabButton>
       </IonTabBar>
