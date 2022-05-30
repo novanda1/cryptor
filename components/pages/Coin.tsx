b42019d1f33f0cac0d187855fa73c6dff69d3195
Novanda Ahsan <novandaahsan1@gmail.com>
2022-05-30 21:25:34 +0700

    better single coin page

--- components/pages/Coin.tsx
+++ components/pages/Coin.tsx
@@ -1,18 +1,33 @@
 import {
-    IonBackButton,
-    IonButtons,
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonToolbar
+  IonBackButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonToolbar
 } from '@ionic/react';
+import Image from 'next/image';
 import { RouteComponentProps } from 'react-router-dom';
+import useCoin from '../../hooks/useCoin';
 import styles from '../ui/Border.module.css';
 import Container from '../ui/Container';
-import PageTitle from '../ui/PageTitle';
 import PX from '../ui/Px';
 
-const Coin: React.FC<RouteComponentProps> = () => {
+const Coin: React.FC<RouteComponentProps<{ id: string }>> = ({
+  match: {
+    params: { id },
+  },
+}) => {
+  const { coin } = useCoin({
+    id,
+    localization: false,
+    tickers: false,
+    community_data: false,
+    developer_data: false,
+    market_data: false,
+    sparkline: false,
+  });
+
   return (
     <IonPage>
       <IonHeader className={styles.HeaderNoBorder} mode="md">
@@ -24,9 +39,24 @@ const Coin: React.FC<RouteComponentProps> = () => {
       </IonHeader>
       <IonContent fullscreen>
         <Container>
-          <PX size={4}>
-            <PageTitle>👋Coin</PageTitle>
-          </PX>
+          {coin ? (
+            <PX size={4}>
+              <div className="flex items-center justify-between mt-5  ">
+                <div className="w-auto">
+                  <h4 className="font-medium">{coin?.symbol.toUpperCase()}</h4>
+                  <h1 className="font-bold text-xl">{coin?.name}</h1>
+                  <p className="text-sm font-medium">
+                    Last close ${coin?.market_data.current_price['usd']}
+                  </p>
+                </div>
+                <div className="w-max">
+                  <Image width={60} height={60} src={coin.image.large} alt={coin.name} />
+                </div>
+              </div>
+            </PX>
+          ) : (
+            'loading...'
+          )}
         </Container>
       </IonContent>
     </IonPage>
